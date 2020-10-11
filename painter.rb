fa7ae3464c8da9a45b465328408cb87d7a0c4171
Paul Lambert <paul.lambert@linux.com>
2020-10-11 07:28:01 -0500

    updating to reflect weekends being weekends

--- painter.rb
+++ painter.rb
@@ -8,15 +8,25 @@ require 'date'
 
 EMAIL = "paul.lambert@linux.com"
 USERNAME = "Paul Lambert"
-NUM_PAST_DAYS = 365*2 # two years of history
+PATH = "/home/paul/code/fake-commits"
+NUM_PAST_DAYS = 365*6 # six years of history
 
-repo = Rugged::Repository.new('.')
+repo = Rugged::Repository.new(PATH)
 
 NUM_PAST_DAYS.times do |x|
 
-  daily_commits = rand(1..15)
+  time_stamp = Time.now - x*24*60*60 
+
+  if time_stamp.saturday? || time_stamp.sunday?
+    daily_commits = rand(0..6)
+  elsif time_stamp.sunday?
+    daily_commits = rand(0..3)
+  else
+    daily_commits = rand(11..31)
+  end
+
   daily_commits.times do |y|
-    time_stamp = Time.now - x*24*60*60 - y*10
+    time_stamp -= y * 10
 
     oid = repo.write("This is a phony timestamp: #{time_stamp}", :blob)
     index = repo.index
